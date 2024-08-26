32c64fe00fab954c0999a70e53aa767639ec2d99
leticia2804 moreira <leticia.mads@outlook.com>
2024-08-26 21:47:40 +0000

    Adicionando o JavaScript nas questões do QUIZ

---
+++ QUIZ/QUESTÕES/questões.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const correctAnswers = {
+        question1: 'Mercurio',
+        question2: 'Netuno',
+        question3: 'Urano',
+        question4: 'Lua',
+        question5: 'Sol',
+        question6: '1969',
+        question7: 'Constelacao',
+        question8: 'Hidrogenio e Helio',
+        question9: 'Meteorito',
+        question10: 'Horizonte de Eventos'
+    };
+
+    function updateAnswerStyles() {
+        const questions = document.querySelectorAll('.card');
+        
+        questions.forEach(card => {
+            const questionId = card.id;
+            const correctAnswer = correctAnswers[`question${questionId.replace('q', '')}`];
+            
+            const inputs = card.querySelectorAll('input[type="radio"]');
+            inputs.forEach(input => {
+                const label = input.parentElement;
+                
+                if (input.checked) {
+                    if (input.value === correctAnswer) {
+                        label.style.color = 'green';  
+                        label.querySelector('input').style.backgroundColor = 'green'; 
+                    } else {
+                        label.style.color = 'red';  
+                        label.querySelector('input').style.backgroundColor = 'red';
+                    }
+                } else {
+                   
+                    label.style.color = '';
+                    label.querySelector('input').style.backgroundColor = '';
+                }
+            });
+        });
+    }
+
+    document.querySelectorAll('input[type="radio"]').forEach(input => {
+        input.addEventListener('change', updateAnswerStyles);
+    });
+
+
+    updateAnswerStyles();
+});
