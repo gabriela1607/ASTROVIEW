ae094404e4f33bdbcf42daf7b1e2c079e4a1777c
Danielle Freitas <135073652+daniellefreitas23@users.noreply.github.com>
2024-09-11 16:24:19 -0300

    arquivo das perguntas

---
+++ QUIZ/QUESTÕES/dados.js
@@ -0,0 +1,82 @@
+export const perguntas = [
+    {
+        pergunta: "1) Qual é o planeta mais próximo do sol?",
+        resposta: [
+            "Mercurio",
+            "Marte",
+            "Venus"
+        ]
+    },
+    {
+        pergunta: "2) Qual o planeta mais distante do sol em nosso sistema solar?",
+        resposta: [
+            "Urano",
+            "Netuno",
+            "Plutao"
+        ],
+    },
+    {
+        pergunta: "3) Qual é o único planeta do sistema solar que gira de lado?",
+        resposta: [
+            "Urano",
+            "Saturno",
+            "Marte"
+        ]
+    },
+    {
+        pergunta: "4) Qual o satélite natural do planeta Terra?",
+        resposta: [
+            "Sol",
+            "Netuno",
+            "Lua"
+        ]
+    },
+    {
+        pergunta: "5) O Sistema Solar possui uma estrela principal, qual o nome dela?",
+        resposta: [
+            "UraLuano",
+            "Sol",
+            "Plutao"
+        ]
+    },
+    {
+        pergunta: "6) Qual foi o ano que o homem pisou na Lua pela primeira vez?",
+        resposta: [
+            "1969",
+            "1912",
+            "1964"
+        ]
+    },
+    {
+        pergunta: "7) Como se chama um aglomerado de estrelas?",
+        resposta: [
+            "Sistema Solar",
+            "Planetoide",
+            "Constelação"
+        ]
+    },
+    {
+        pergunta: "8) Qual são os dois principais componentes do Sol?",
+        resposta: [
+            "Gas Carbonico e Helio",
+            "Ferro e Níquel",
+            "Hidrogenio e Helio"
+        ]
+    },
+    {
+        "pergunta": "9) O que é uma estrela cadente?",
+        "resposta": [
+            "Reflexo da Lua",
+            "Estrela anã",
+            "Meteorito"
+        ]
+    },
+    {
+        "pergunta": "10 ) Qual o nome do centro de um buraco negro de onde a luz é incapaz de sair?",
+        "resposta": [
+            "Horizonte de Eventos",
+            "Centro gravitacional",
+            "Espaço tempo"
+        ]
+    }
+]
