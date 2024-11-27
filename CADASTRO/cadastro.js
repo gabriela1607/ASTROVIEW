6eb5a35ce66c77ffeccfb6af7ef9db2a44f1694a
Danielle Freitas <135073652+daniellefreitas23@users.noreply.github.com>
2024-11-26 21:53:29 -0300

    Update cadastro.js

--- CADASTRO/cadastro.js
+++ CADASTRO/cadastro.js
@@ -1,16 +1,17 @@
 document.getElementById('cadastro-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const nome = e.target[0].value;
-    const email = e.target[1].value;
-    const senha = e.target[2].value;
+    const nome = document.getElementById("registername").target[0].value;
+    const sobrenome = document.getElementById("registersobrenome").target[1].value;
+    const email = document.getElementById("registeremail").target[2].value;
+    const senha = document.getElementById("registerPassword").target[3].value;
 
     fetch('http://localhost:3000/api/cadastrar', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ nome, email, senha })
+        body: JSON.stringify({ nome,sobrenome, email, senha })
     })
     .then(response => response.json())
     .then(data => {
